537a6a84e7608d38f217d60c386728d9709fbc8d
WailTech <116460489+WailApp@users.noreply.github.com>
2024-08-15 12:37:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,15 @@
 <?php
 // إعداد المتغيرات
 $buildNumber = isset($_GET['build']) ? $_GET['build'] : '';
-$updateName = '';
-$updateChangelog = '';
-$updateCategory = '';
-
-// تحقق من رقم البناء وإعداد المعلومات المناسبة
-if ($buildNumber === '1.0') {
-    $updateName = 'Update 1.0'; // اسم التحديث
-    $updateChangelog = 'Bug fixes and improvements.'; // سجل التغييرات
-    $updateCategory = 'Important'; // فئة التحديث
-} elseif ($buildNumber === '2.0') {
-    $updateName = 'Update 2.0'; // اسم التحديث
-    $updateChangelog = 'New features added.'; // سجل التغييرات
-    $updateCategory = 'Important'; // فئة التحديث
-} else {
+$updateName = '   Repair System';
+$updateChangelog = 'zeqOS will be upgraded to a newer version, and errors will be fixed.';
+$updateCategory = 'Important';
+//} else {
     // رقم البناء غير مدعوم
-    $updateName = 'No new updates';
-    $updateChangelog = 'You\'re up to date! Try checking for updates later.';
-    $updateCategory = 'Up-to-date';
-}
+  //  $updateName = 'No new updates';
+   // $updateChangelog = 'You\'re up to date! Try checking for updates later.';
+   // $updateCategory = 'Up-to-date';
+//}
 
 // إرجاع البيانات كاستجابة نصية مفصولة بعلامة |
 echo $updateName . '|' . $updateChangelog . '|' . $updateCategory;
